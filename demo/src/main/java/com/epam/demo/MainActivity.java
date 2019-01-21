219306168865af74c567c70d30294a4bca4be04e
Denys Mokhrin <denys.mokhrin@gmail.com>
2019-01-21 15:35:38 +0200

    - commit missed files

--- demo/src/main/java/com/epam/demo/MainActivity.java
+++ demo/src/main/java/com/epam/demo/MainActivity.java
@@ -7,52 +7,82 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epam.keystore.SecureStorage;
 import com.epam.keystore.core.SecurityProvider;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.tv_decrypted_value)
-    TextView tvDecryptedValue;
-
-    @BindView(R.id.tv_stored_value)
-    TextView tvStoredValue;
-
-    @BindView(R.id.et_value_to_store)
-    EditText etToBeStored;
-
-    private SecureStorage storage;
-    private final String TEST_KEY = "com.epam.keystore.keystore.demo.TEST_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
-        storage = new SecureStorage(this, SecurityProvider.Type.CIPHER);
-
+        initCipherProvider();
+        initThemisProvider();
     }
 
-    public void saveValue(View view) {
-        if (storage != null) {
-            storage.save(TEST_KEY, etToBeStored.getText().toString());
-        }
+    private void initCipherProvider() {
+        final SecureStorage storage = new SecureStorage(this, SecurityProvider.Type.CIPHER);
+
+        final EditText valueKey = findViewById(R.id.value_key);
+        final EditText valueStore = findViewById(R.id.value_store);
+        final TextView anyValue = findViewById(R.id.tv_decrypted_value);
+
+        findViewById(R.id.btn_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storage.save(valueKey.getText().toString(), valueStore.getText().toString());
+                Toast.makeText(view.getContext(), "Has been saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.btn_get_decrypted_value).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                anyValue.setText(storage.get(valueKey.getText().toString()));
+            }
+        });
+
+        findViewById(R.id.btn_get_stored_value).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(view.getContext());
+                anyValue.setText(prefs.getString(valueKey.getText().toString(), ""));
+            }
+        });
     }
 
-    public void getDecryptedValue(View view) {
-        if (storage != null) {
-            tvDecryptedValue.setText(storage.get(TEST_KEY));
-        }
-    }
+    private void initThemisProvider() {
+        final SecureStorage storage = new SecureStorage(this, SecurityProvider.Type.THEMIS);
 
-    public void getStoredValue(View view) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        tvStoredValue.setText(prefs.getString(TEST_KEY, ""));
-    }
+        final EditText valueKey = findViewById(R.id.value_key2);
+        final EditText valueStore = findViewById(R.id.value_store2);
+        final TextView anyValue = findViewById(R.id.tv_decrypted_value2);
 
+        findViewById(R.id.btn_save2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storage.save(valueKey.getText().toString(), valueStore.getText().toString());
+                Toast.makeText(view.getContext(), "Has been saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.btn_get_decrypted_value2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                anyValue.setText(storage.get(valueKey.getText().toString()));
+            }
+        });
+
+        findViewById(R.id.btn_get_stored_value2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(view.getContext());
+                anyValue.setText(prefs.getString(valueKey.getText().toString(), ""));
+            }
+        });
+    }
 }
